dd0ba2af4833cd124d7338d27efbd3c282f2815b
sunrisepeak <speakshen@163.com>
2026-03-11 04:22:17 +0800

    feat: add llmapi 0.2.1 (fix test deps for package install)

--- packages/l/llmapi/xmake.lua
+++ packages/l/llmapi/xmake.lua
@@ -9,6 +9,7 @@ package("llmapi")
         "https://github.com/mcpplibs/llmapi.git"
     )
 
+    add_versions("0.2.1", "d48f0bd6b6186aac91f5a26e9a20bc59014f396e7589cf8a52dfd6790de31dab")
     add_versions("0.2.0", "2969d573cf50563159f74ac0a65ccaa05db86a5becf4786de6d6daeef8012d90")
     add_versions("0.1.0", "f01f5b0b6d11242b655f7c383e82a71e30a7696cdf4468d7ab3a3ae72f99b3e5")
     add_versions("0.0.1", "174f86d3afdf48a57ad1cc9688718d1f1100a78a7e56686c823c573c3ccf99f4")
